72ca0556cc24f427926305bdd8187169ff6ede75
Ifra Shaikh <ifrashaikh701@gmail.com>
2024-01-10 23:03:10 +0530

    code for table of 5

---
+++ src/Table.java
@@ -0,0 +1,10 @@
+public class Table{
+    public static void main (String[] args)throws Exception{
+        int a;
+         System.out.println("Table of 5 is :");
+        for (a=1;a<=10;a++){
+          
+            System.out.println(5*a);
+        }
+    }
+}
