a097e4be8e850e1c7f3a56e85cfee240a0c51eed
Ifra Shaikh <ifrashaikh701@gmail.com>
2024-01-10 23:48:25 +0530

    ASCII value

---
+++ src/asc.java
@@ -0,0 +1,7 @@
+public class asc{
+    public static void main(String[] args)throws Exception{
+        char alphabet='I';
+        int AsciiValue=(int)alphabet;
+        System.out.println("The ASCII value of '"+alphabet+"' is "+AsciiValue);
+    }
+}
