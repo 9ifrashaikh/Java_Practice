1c759adc4467475ef55a7b0675389a5717e2b30e
Ifra Shaikh <ifrashaikh701@gmail.com>
2024-01-10 17:24:23 +0530

    first commit

---
+++ src/Add.java
@@ -0,0 +1,16 @@
+public class Add{
+    public static void main(String[] args) throws Exception{
+        int num1=3;
+        int num2=4;
+        int sum;
+        sum= num1+num2;
+        int subs=num1-num2;
+        float mult=num1*num2;
+        double div=(double)num1/num2;
+    System.out.println("Summation is "+sum);
+    System.out.println("Subtraction is "+subs);
+    System.out.println("Multiplication is "+mult);
+    System.out.println("Division is "+div);
+}
+}
+// simple java code for diffrent operations  
